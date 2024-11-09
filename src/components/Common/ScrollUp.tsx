17a9a37b09944dcfd660390cbb46776bde787f5e
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-11-09 10:45:50 +0000

    revamping the homepage

--- src/components/Common/ScrollUp.tsx
+++ src/components/Common/ScrollUp.tsx
@@ -7,3 +7,5 @@ export default function ScrollUp() {
 
   return null;
 }
+
+
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,10 +12,10 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                 Empowering your digital world
+                 Empowering your digital world...
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                100 million people find their ideal digital solutions
+                100 million people find their ideal digital solutions with Bainaryglobe
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
