5bfea674db48a1cbb06e0ddbb57d8d053423246f
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-11-12 18:33:48 +0000

    alter lorem ipsum

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -14,7 +14,7 @@ const Video = () => {
       <div className="container">
         <SectionTitle
           title="We are ready to help"
-          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+          paragraph="Bainaryglobe to the rescue!."
           center
           mb="80px"
         />
