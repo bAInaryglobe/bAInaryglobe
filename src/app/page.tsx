58209b09621f7903fc4627787d450b356f2b0029
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-11-19 15:27:44 +0100

    add metadata comment for clarity in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import Testimonials from "@/components/Testimonials";
 import Video from "@/components/Video";
 import { Metadata } from "next";
 
-
+// metadata for pages
 export const metadata: Metadata = {
   title: "bAInaryglobe",
   description: "This is Home for bAInaryglobe website",
